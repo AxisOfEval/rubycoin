1100f4218e81b2d450d9e92da9d2ea578457a580
Amol Hatwar <amol@hatwar.org>
2014-02-23 23:12:00 +0530

    Added constants for public and private keys

--- lib/ruby_coin/bitcoin.rb
+++ lib/ruby_coin/bitcoin.rb
@@ -4,5 +4,7 @@ module RubyCoin
   module Bitcoin
     CURVE_TYPE = 'secp256k1'
     ADDR_CHARS = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+    PUBKEY_VER = '00'
+    PRIKEY_VER = '80'
   end
 end
